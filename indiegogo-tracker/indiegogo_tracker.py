a91c73e65d8603123428bee10fe47a96974c3445
Erkki Mattila <lasokki@gmail.com>
2014-12-22 21:20:31 +0200

    Refactored the script into functions. Needs still work

--- indiegogo-tracker/indiegogo_tracker.py
+++ indiegogo-tracker/indiegogo_tracker.py
@@ -10,25 +10,18 @@ Author: Erkki Mattila, 2014
 
 import requests, re, time, sys, BeautifulSoup, datetime
 
-numpa = re.compile('\d+')
-session = requests.session()
-url = sys.argv[1]
-
-old_balance = 0
-balance = 0
-first_run = True
-
-while True:
+def get_doc(url):
     req = session.get(url)
-
     doc = BeautifulSoup.BeautifulSoup(req.content)
+    return doc
 
-    title = doc.find("h1")
+def search_balance(doc):
+
+    numpa = re.compile('\d+')
     
     bal_div = doc.find("div", attrs={"class" : "i-balance"})
     bal_span = bal_div.find("span", attrs={"class" : "currency currency-xlarge"})
 
-    old_balance = balance
     dollar_format_balstr = bal_span.contents[0].string
     
     ballist = numpa.findall(dollar_format_balstr)
@@ -39,15 +32,29 @@ while True:
 
     balance = int(balstring)
 
-    if first_run == True:
-        old_balance = balance
-        first_run = False
+    return balance
 
+def print_balance(balance, old_balance, title):
     delta_bal_str = ""
     if balance > old_balance:
         delta_balance = balance - old_balance
         delta_bal_str = " | +$" + str(delta_balance)
-
+        
     print title.string
-    print datetime.datetime.now().strftime("%H:%M:%S") + " | " + dollar_format_balstr + delta_bal_str
-    time.sleep(float(sys.argv[2]))
+    print datetime.datetime.now().strftime("%H:%M:%S") + " | " + str(balance) + delta_bal_str
+
+if __name__ == "__main__":
+    session = requests.session()
+    url = sys.argv[1]
+    delay = float(sys.argv[2])
+
+    old_balance = 0
+    balance = 0
+
+    while True:
+        doc = get_doc(url)
+        balance = search_balance(doc)
+        title = doc.find("h1")
+        print_balance(balance, old_balance, title)
+        old_balance = balance
+        time.sleep(delay)
